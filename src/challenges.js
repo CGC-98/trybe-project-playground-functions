b1a9bc4cc04b7cad82b494d0cd65ca2768bb9c11
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-24 20:10:16 -0300

    challenge 8 ESlint absention

--- src/challenges.js
+++ src/challenges.js
@@ -62,9 +62,31 @@ function catAndMouse(rat, cat1, cat2) {
   return output;
 }
 
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 // Desafio 8
-function fizzBuzz() {
+function fizzBuzz(numberArray) {
   // seu código aqui
+  let flag = [];
+  for (let i in numberArray) {
+    if (nothing(numberArray[i])) {
+      flag.push('bug!');
+    } if (double(numberArray[i])) {
+      flag.push('fizzBuzz');
+    } else if (numberArray[i] % 3 === 0) {
+      flag.push('fizz');
+    } else if (numberArray[i] % 5 === 0) {
+      flag.push('buzz')
+    }
+  }
+  return flag;
+}
+
+function double(x) {
+  return (x % 3 === 0 && x % 5 === 0);
+}
+
+function nothing(y) {
+  return (y % 3 !== 0 && y % 5 !== 0);
 }
 
 // Desafio 9
