046eac5a00d5f4ff3cc975e65f900653347bdbd1
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-25 12:54:05 -0300

    challenge 11 chaos completion

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,6 +1,38 @@
+console.log(generatePhoneNumber([0, 0, 0, 2, 3, 4, 5, 6, 7, 8, 9]))
+
 // Desafio 11
-function generatePhoneNumber() {
+function generatePhoneNumber(x) {
   // seu código aqui
+  // '(XX) XXXXX-XXXX';
+  let pattern = '(';
+  let amount = {};
+  if (x.length < 11 || x.length > 11) {
+    return 'Array com tamanho incorreto.';
+  } else {
+    for (let i in x) {
+      if (!amount[x[i]]) {
+        amount[x[i]] = 1;
+      } else {
+        amount[x[i]] += 1;
+      } if (amount[x[i]] > 2) {
+        return 'não é possível gerar um número de telefone com esses valores'
+      } else if (x[i] < 0 || x[i] > 9) {
+        return 'não é possível gerar um número de telefone com esses valores';
+      } else if (i < 2) {
+        pattern += x[i];
+      } else if (i == 2) {
+        pattern += ') ';
+        pattern += x[i];
+      } else if (i > 2 && i < 7) {
+        pattern += x[i];
+      } else if (i == 7) {
+        pattern += '-';
+        pattern += x[i];
+      } else if (i > 7) {
+        pattern += x[i];
+      }
+    }
+  } return pattern;
 }
 
 // Desafio 12
