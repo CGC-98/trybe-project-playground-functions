4ac2d8957f026612494c2de4eb48d49b85f75dc8
CGC-98 <caiquecorreia.gama@hotmail.com>
2022-08-25 14:54:55 -0300

    challenge 11 rewrite

--- src/challenges2.js
+++ src/challenges2.js
@@ -1,39 +1,45 @@
-console.log(generatePhoneNumber([0, 0, 0, 2, 3, 4, 5, 6, 7, 8, 9]))
-
 // Desafio 11
 function generatePhoneNumber(x) {
   // seu código aqui
-  // '(XX) XXXXX-XXXX';
   let pattern = '(';
   let amount = {};
   if (x.length < 11 || x.length > 11) {
-    return 'Array com tamanho incorreto.';
+    pattern = 'Array com tamanho incorreto.';
   } else {
     for (let i in x) {
-      if (!amount[x[i]]) {
-        amount[x[i]] = 1;
+      if (!pattern) {
+        pattern = false
       } else {
-        amount[x[i]] += 1;
-      } if (amount[x[i]] > 2) {
-        return 'não é possível gerar um número de telefone com esses valores'
-      } else if (x[i] < 0 || x[i] > 9) {
-        return 'não é possível gerar um número de telefone com esses valores';
-      } else if (i < 2) {
-        pattern += x[i];
-      } else if (i == 2) {
-        pattern += ') ';
-        pattern += x[i];
-      } else if (i > 2 && i < 7) {
-        pattern += x[i];
-      } else if (i == 7) {
-        pattern += '-';
-        pattern += x[i];
-      } else if (i > 7) {
-        pattern += x[i];
+        if (!amount[x[i]]) {
+          amount[x[i]] = 1;
+        } else {
+          amount[x[i]] += 1;
+        } if (amount[x[i]] > 2) {
+          pattern = false;
+        } else if (x[i] < 0 || x[i] > 9) {
+          pattern = false;
+        } else if (i < 2) {
+          pattern += x[i];
+        } else if (i == 2) {
+          pattern += ') ';
+          pattern += x[i];
+        } else if (i > 2 && i < 7) {
+          pattern += x[i];
+        } else if (i == 7) {
+          pattern += '-';
+          pattern += x[i];
+        } else if (i > 7) {
+          pattern += x[i];
+        }
       }
     }
-  } return pattern;
+  } if (!pattern) {
+    pattern = 'não é possível gerar um número de telefone com esses valores';
+  }
+  return pattern;
 }
+
+console.log(generatePhoneNumber([0, 0, 0, 2, 3, 4, 5, 6, 7, 8, 9]));
 
 // Desafio 12
 function triangleCheck(lineA, lineB, lineC) {
@@ -48,8 +54,10 @@ function hydrate(drink) {
   let water = 0;
   let tip = '';
   for (let i in regex) {
-    water += Number(regex[i]);
-  } if (water != 1) {
+    if (regex.length > 0) {
+      water += Number(regex[i]);
+    }
+  } if (water !== 1) {
     tip += water;
     tip += ' copos de água';
   } else {
@@ -57,7 +65,7 @@ function hydrate(drink) {
   } return tip;
 }
 
-console.log(hydrate('01 a, 5 b e 1 c'));
+console.log(hydrate('01 a'));
 
 module.exports = {
   generatePhoneNumber,
